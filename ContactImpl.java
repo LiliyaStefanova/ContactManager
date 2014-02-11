3670c4c81f5a631578852f3a227df3bbda478190
Ejko <liliya.stefanova@gmail.com>
2014-02-11 15:40:51 +0000

    added required fields and a constructor to the implementation

--- ContactImpl.java
+++ ContactImpl.java
@@ -6,6 +6,17 @@
  * To change this template use File | Settings | File Templates.
  */
 public class ContactImpl implements Contact {
+
+    private int contactID;
+    private String contactName;
+    private String contactNotes;
+
+    public ContactImpl(int id, String name, String notes){
+        this.contactID=id;
+        this.contactName=name;
+        this.contactNotes=notes;
+    }
+
     @Override
     public int getId() {
         return 0;  //To change body of implemented methods use File | Settings | File Templates.
