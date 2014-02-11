8706916f9c96f160dd3a3979bc25177c945e9a9f
Ejko <liliya.stefanova@gmail.com>
2014-02-11 18:16:53 +0000

    created tests which fail for MeetingImpl.java

--- MeetingTest.java
+++ MeetingTest.java
@@ -6,23 +6,53 @@
  * To change this template use File | Settings | File Templates.
  */
 import org.junit.*;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MeetingTest {
 
+    Meeting testMeeting;
+    Set<Contact> testContact;
+
+    @Before
+    public void buildUp(){
+        testContact=new HashSet<Contact>();
+        Contact contact1=new ContactImpl(1234, "Jane", "venture capital");
+        Contact contact2=new ContactImpl(3456, "Henry", "investor");
+        testContact.add(contact1);
+        testContact.add(contact2);
+        Calendar meetingDate=Calendar.getInstance();
+        meetingDate.set(2012,Calendar.DECEMBER,11);
+
+        testMeeting=new MeetingImpl(4567,meetingDate, testContact);
+    }
 
     @Test
     public void getMeetingIdTest(){
 
+        assertTrue(testMeeting.getId()==4567);
     }
 
     @Test
     public void getMeetingDateTest(){
+        Calendar expectedDate=Calendar.getInstance();
+        expectedDate.set(2012,Calendar.DECEMBER,11);
+        assertTrue(testMeeting.getDate().equals(expectedDate));
 
     }
 
     @Test
     public void getMeetingContactsTest(){
+        Set<Contact> expectedContacts=new HashSet<Contact>();
+        Contact contact1=new ContactImpl(1234, "Jane", "venture capital");
+        Contact contact2=new ContactImpl(3456, "Henry", "investor");
+        expectedContacts.add(contact1);
+        expectedContacts.add(contact2);
+
+        assertTrue(expectedContacts.containsAll(testMeeting.getContacts()));
 
     }
 
