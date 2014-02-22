d1703c1eb6bc87639aecfd0c311a7c8d5c85e54d
Ejko <liliya.stefanova@gmail.com>
2014-02-22 18:27:00 +0000

    added hashcode and equals method implementations to improve comparison

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -23,6 +23,28 @@ public class MeetingImpl implements Meeting {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MeetingImpl)) return false;
+
+        MeetingImpl meeting = (MeetingImpl) o;
+
+        if (meetingID != meeting.meetingID) return false;
+        if (!meetingAttendees.equals(meeting.meetingAttendees)) return false;
+        if (!scheduledDate.equals(meeting.scheduledDate)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = meetingID;
+        result = 31 * result + scheduledDate.hashCode();
+        result = 31 * result + meetingAttendees.hashCode();
+        return result;
+    }
+
+    @Override
     public int getId() {
 
         return this.meetingID;
@@ -39,29 +61,5 @@ public class MeetingImpl implements Meeting {
 
         return this.meetingAttendees;
     }
-    /**
-     * Testing purposes only-to be removed
-    public static void main(String [] args){
-        Set<Contact> testContact=new HashSet<Contact>();
-        Contact contact1=new ContactImpl(1234, "Jane", "venture capital");
-        Contact contact2=new ContactImpl(3456, "Henry", "investor");
-        testContact.add(contact1);
-        testContact.add(contact2);
-        Calendar meetingDate=Calendar.getInstance();
-        meetingDate.set(2012,Calendar.DECEMBER,11);
-        Meeting meet=new MeetingImpl(1234,meetingDate, testContact);
-        Set<Contact> test1=new HashSet<Contact>();
-        Contact contact3=new ContactImpl(1234, "Jane", "venture capital");
-        Contact contact4=new ContactImpl(3456, "Henry", "investor");
-        test1.add(contact1);
-        test1.add(contact2);
-        for(Contact current:test1){
-
-            System.out.println(current.getName());
-        }
-            System.out.println(testContact.equals(test1));
-
-        }
-    */
 
 }
